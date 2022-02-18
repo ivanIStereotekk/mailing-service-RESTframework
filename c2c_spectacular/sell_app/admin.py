8b1661edad45c568360e7b4681554ac7b4b97021
Bodybro <ivan.stereotekk@gmail.com>
2022-02-18 12:56:39 +0400

    Done ! Done ! Done !

--- c2c_spectacular/sell_app/admin.py
+++ c2c_spectacular/sell_app/admin.py
@@ -10,6 +10,7 @@ class Customer_Admin(admin.ModelAdmin):
         list_display = ()
         list_display_links = ('username', 'user_type','phone_number')
         search_fields = ('user_type','username','phone_number',)
+        list_filter = ('username', 'phone_number',)
 
 admin.site.register(Customer, Customer_Admin)
 
@@ -18,7 +19,8 @@ class Good_Admin(admin.ModelAdmin):
     class Meta:
         list_display = ('customer','code','description','quantity')
         list_display_links = ('customer','code',)
-        search_fields = ('customer','code',)
+        search_fields = ('customer','code','description','quantity')
+        list_filter = ('customer','code',)
 admin.site.register(Good, Good_Admin)
 
 
